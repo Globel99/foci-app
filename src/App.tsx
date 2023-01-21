acea4b2ac05e50366c3f1771a6c31a9689a45eb7
leventekapitany <kap.levente@gmail.com>
2023-01-21 02:50:04 +0100

    app and navigator styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import { Context, Theme } from './context';
+import styled from 'styled-components';
 
 import LastMatches from './views/last-matches';
 import Navigator from './components/navigator';
@@ -18,15 +19,41 @@ function App() {
 
   return (
     <Context.Provider value={{ theme, setTheme }}>
-      <div id="app" className={isDark ? 'dark' : ''}>
-        <div id="container">
+      <AppRoot id="app" className={isDark ? 'dark' : ''}>
+        <Container id="container">
           <img src={logoPath} />
           <LastMatches />
-        </div>
+        </Container>
         <Navigator />
-      </div>
+      </AppRoot>
     </Context.Provider>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  position: absolute;
+  height: calc(100% - 50px);
+  overflow-y: scroll;
+  max-width: 600px;
+  flex-flow: column;
+  font-family: var(--font-family);
+  box-sizing: border-box;
+  padding: 8px;
+`;
+
+const AppRoot = styled.div`
+  display: grid;
+  place-items: center;
+  background-color: var(--bg);
+  color: var(--font);
+  transition: all 0.1s ease-out;
+
+  #container {
+    top: 50px;
+  }
+}
+`;
 
 export default App;
